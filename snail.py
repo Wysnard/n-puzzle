bfede4f81ad2de09174b3f65988bc75b27f41527
Vincent Lay <wysnard@wysnard-TM1701>
2019-09-18 00:36:06 +0200

    go go

--- snail.py
+++ snail.py
@@ -1,40 +1,41 @@
 def spiral_traversal(matrix):
-	res = []
-	if len(matrix) == 0:
-		return res
-	row_begin = 0
-	row_end = len(matrix) - 1
-	col_begin = 0
-	col_end = len(matrix[0]) - 1
+    res = []
+    if len(matrix) == 0:
+        return res
+    row_begin = 0
+    row_end = len(matrix) - 1
+    col_begin = 0
+    col_end = len(matrix[0]) - 1
 
-	while row_begin <= row_end and col_begin <= col_end:
-		print("#1")
-		for i in range(col_begin, col_end+1):
-			print(col_begin, i, ':', matrix[row_begin][i])
-			res.append(matrix[row_begin][i])
-		row_begin += 1
+    while row_begin <= row_end and col_begin <= col_end:
+        print("#1")
+        for i in range(col_begin, col_end+1):
+            print(col_begin, i, ':', matrix[row_begin][i])
+            res.append(matrix[row_begin][i])
+        row_begin += 1
 
-		print('#2')
-		for i in range(row_begin, row_end+1):
-			print(i, col_end, ':', matrix[i][col_end])
-			res.append(matrix[i][col_end])
-		col_end -= 1
+        print('#2')
+        for i in range(row_begin, row_end+1):
+            print(i, col_end, ':', matrix[i][col_end])
+            res.append(matrix[i][col_end])
+        col_end -= 1
 
-		print('#3')
-		if row_begin <= row_end:
-			for i in range(col_end, col_begin-1, -1):
-				print(row_end, i, ':', matrix[row_end][i])
-				res.append(matrix[row_end][i])
-		row_end -= 1
+        print('#3')
+        if row_begin <= row_end:
+            for i in range(col_end, col_begin-1, -1):
+                print(row_end, i, ':', matrix[row_end][i])
+                res.append(matrix[row_end][i])
+        row_end -= 1
 
-		print('#4')
-		if col_begin <= col_end:
-			for i in range(row_end, row_begin-1, -1):
-				print(i, col_begin, ':', matrix[i][col_begin])
-				res.append(matrix[i][col_begin])
-		col_begin += 1
+        print('#4')
+        if col_begin <= col_end:
+            for i in range(row_end, row_begin-1, -1):
+                print(i, col_begin, ':', matrix[i][col_begin])
+                res.append(matrix[i][col_begin])
+        col_begin += 1
 
-	return res
+    return res
+
 
 mat = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::error::Error;
 use std::fmt;
 use std::rc::Rc;
@@ -21,22 +22,25 @@ impl Heuristique {
             .iter()
             .enumerate()
             .map(|(y, line)| {
-                line.iter().enumerate().fold(0f64, |acc, (x, col)| {
-                    let g = goal
-                        .iter()
-                        .enumerate()
-                        .filter(|(_, l)| l.iter().any(|c| c == col))
-                        .fold((0f64, 0f64), |_, (g_y, l)| {
-                            (
-                                g_y as f64,
-                                l.iter().enumerate().find(|c| c.1 == col).unwrap().0 as f64,
-                            )
-                        });
-                    acc + (g.0 - y as f64).abs() + (g.1 - x as f64).abs()
-                })
+                line.iter()
+                    .enumerate()
+                    .filter(|(_, col)| **col != 0)
+                    .fold(0f64, |acc, (x, col)| {
+                        let g = goal
+                            .iter()
+                            .enumerate()
+                            .filter(|(_, l)| l.iter().any(|c| c == col))
+                            .fold((0f64, 0f64), |_, (g_y, l)| {
+                                (
+                                    g_y as f64,
+                                    l.iter().enumerate().find(|c| c.1 == col).unwrap().0 as f64,
+                                )
+                            });
+                        acc + (g.0 - y as f64).abs() + (g.1 - x as f64).abs()
+                    })
             })
             .sum();
-        println!("HEURISTIQUE RES : {:?}", res);
+        // println!("HEURISTIQUE RES : {:?}", res);
         res
     }
 }
@@ -46,27 +50,39 @@ pub struct Node {
     grid: Vec<Vec<i64>>,
     g: f64,
     h: f64,
-    parent: Rc<Option<Node>>,
+    parent: Option<Rc<Node>>,
 }
 
 impl Node {
     fn new(
         grid: Vec<Vec<i64>>,
-        parent: Option<Node>,
+        parent: Option<Rc<Node>>,
         goal: &Vec<Vec<i64>>,
         heuristique: &Heuristique,
     ) -> Node {
-        let gplus = parent.clone();
-        let gplus = match gplus {
-            Some(n) => n.g,
+        let g = parent.clone();
+        let g = match g {
+            Some(n) => n.g + 1.0,
             None => 0.0,
         };
-        Node {
-            grid,
-            h: 0.0,
-            g: 0.0 + gplus,
-            parent: Rc::new(parent),
-        }
+        let h = heuristique.process_h(&grid, goal);
+        Node { grid, h, g, parent }
+    }
+
+    fn get_f(&self) -> f64 {
+        return self.g + self.h;
+    }
+}
+
+impl PartialEq for Node {
+    fn eq(&self, other: &Node) -> bool {
+        return self.get_f() == other.get_f();
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Node) -> Option<Ordering> {
+        self.get_f().partial_cmp(&other.get_f())
     }
 }
 
@@ -94,7 +110,7 @@ pub struct NPuzzle {
     pub goal: Vec<Vec<i64>>,
     pub heuristique: Heuristique,
     pub open_list: Vec<Node>,
-    pub close_list: Vec<Node>,
+    pub close_list: Vec<Rc<Node>>,
 }
 
 impl NPuzzle {
@@ -104,8 +120,8 @@ impl NPuzzle {
             .map(|x| x.split('#').next().unwrap().trim())
             .filter(|x| !x.is_empty())
             .map(|x| {
-                x.split(' ')
-                    .map(|x| if x == "0" { "9223372036854775807" } else { x })
+                x.split_whitespace()
+                    // .map(|x| if x == "0" { "9223372036854775807" } else { x })
                     .map(|x| x.parse::<i64>())
                     .collect()
             })
@@ -138,70 +154,163 @@ impl NPuzzle {
     }
 
     pub fn generate_goal(size: &i64, map: &Vec<Vec<i64>>) -> Vec<Vec<i64>> {
-        let mut map: Vec<i64> = map.clone().into_iter().flatten().collect();
+        let mut map: Vec<i64> = map
+            .clone()
+            .into_iter()
+            .flatten()
+            .map(|x| if x == 0 { 9223372036854775807 } else { x })
+            .collect();
         map.sort();
 
-        let mut goal: Vec<i64> = Vec::new();
-        let mut row_begin = 0;
-        let mut row_end = *size - 1;
-        let mut col_begin = 0;
-        let mut col_end = *size - 1;
+        let mut map = map
+            .iter()
+            .map(|x| if x == &9223372036854775807 { &0 } else { x });
 
-        println!("SORTED : {:?}", map);
-        while row_begin <= row_end && col_begin <= col_end {
-            println!("#1");
-            for i in col_begin..=col_end {
-                println!(
-                    "{} {} : {}",
-                    row_begin,
-                    i,
-                    map[(row_begin * size + i) as usize]
-                );
-                goal.push(map[(row_begin * size + i) as usize]);
-            }
-            row_begin += 1;
+        let mut A: Vec<Vec<i64>> = vec![vec![0; *size as usize]; *size as usize];
+        let n = *size;
+        let mut len = *size;
+        let mut k = 0;
+        let mut p = 0;
 
-            println!("#2");
-            for i in row_begin..=row_end {
-                println!("{} {} : {}", i, col_end, map[(i * size + col_end) as usize]);
-                goal.push(map[(i * size + col_end) as usize]);
+        while k < n * n {
+            // println!("#1");
+            for i in p..len {
+                // println!("{} {}", p, i);
+                A[p as usize][i as usize] = *map.next().unwrap();
+                k += 1;
             }
-            col_end -= 1;
 
-            println!("#3");
-            if row_begin <= row_end {
-                for i in (col_begin..=col_end).rev() {
-                    println!("{} {} : {}", row_end, i, map[(row_end * size + i) as usize]);
-                    goal.push(map[(row_end * size + i) as usize]);
-                }
+            // println!("#2");
+            for i in p + 1..len {
+                A[i as usize][(len - 1) as usize] = *map.next().unwrap();
+                // println!("{} {} : {}", i, len, A[i as usize][(len - 1) as usize]);
+                k += 1;
             }
-            row_end -= 1;
 
-            println!("#4");
-            if col_begin <= col_end {
-                for i in (row_begin..=row_end).rev() {
-                    println!(
-                        "{} {} : {}",
-                        i,
-                        col_begin,
-                        map[(i * size + col_begin) as usize]
-                    );
-                    goal.push(map[(i * size + col_begin) as usize]);
-                }
+            // println!("#3");
+            for i in (p..len - 1).rev() {
+                A[(len - 1) as usize][i as usize] = *map.next().unwrap();
+                // println!("{} {} : {}", len - 1, i, A[(len - 1) as usize][i as usize]);
+                k += 1
             }
-            col_begin += 1;
+
+            // println!("#4");
+            for i in (p + 1..len - 1).rev() {
+                A[i as usize][p as usize] = *map.next().unwrap();
+                // println!("{} {} : {}", i, p, A[i as usize][p as usize]);
+                k += 1;
+            }
+
+            p += 1;
+            len -= 1;
         }
-        println!("GOAL : {:?}", goal);
-
-        let mut res: Vec<Vec<i64>> = vec![vec![0; *size as usize]; *size as usize];
-        for (i, s) in goal.iter().enumerate() {
-            res[i / *size as usize][i % *size as usize] = *s;
-        }
-        // println!("RES : {:?}", res);
-        res
+        println!("A : {:?}", A);
+        A
     }
 
     pub fn run(&mut self) {
         println!("RUN !");
+        let mut i = 0;
+        let solved = loop {
+            i += 1;
+            let current = self.open_list.remove(0);
+
+            if current.h == 0.0 {
+                break current;
+            }
+
+            println!("OPEN LIST : {:?}", self.open_list);
+            println!("CURRENT : {:?}", current);
+
+            let pos = current
+                .grid
+                .iter()
+                .enumerate()
+                .fold((0usize, 0usize), |acc, (i, x)| {
+                    if x.iter().any(|&y| y == 0) {
+                        (i, x.iter().position(|&y| y == 0).unwrap())
+                    } else {
+                        acc
+                    }
+                });
+            println!("POS: {:?}", pos);
+
+            let mut swaps: Vec<Node> = vec![];
+            let current_grid = current.grid.clone();
+            let goal = self.goal.clone();
+            let parent = Rc::new(current);
+            if pos.0 as i32 > 0 {
+                println!("TOP SWAP !");
+                let mut top = current_grid.clone();
+                top[pos.0][pos.1] = top[pos.0 - 1][pos.1];
+                top[pos.0 - 1][pos.1] = 0;
+                swaps.push(Node::new(
+                    top,
+                    Some(parent.clone()),
+                    &goal,
+                    &self.heuristique,
+                ));
+            }
+
+            if pos.1 + 1 < self.size as usize {
+                println!("RIGHT SWAP !");
+                let mut right = current_grid.clone();
+                right[pos.0][pos.1] = right[pos.0][pos.1 + 1];
+                right[pos.0][pos.1 + 1] = 0;
+                swaps.push(Node::new(
+                    right,
+                    Some(parent.clone()),
+                    &goal,
+                    &self.heuristique,
+                ));
+            }
+
+            if pos.0 + 1 < self.size as usize {
+                println!("BOTTOM SWAP !");
+                let mut bottom = current_grid.clone();
+                bottom[pos.0][pos.1] = bottom[pos.0 + 1][pos.1];
+                bottom[pos.0 + 1][pos.1] = 0;
+                swaps.push(Node::new(
+                    bottom,
+                    Some(parent.clone()),
+                    &goal,
+                    &self.heuristique,
+                ));
+            }
+
+            if pos.1 as i32 > 0 {
+                println!("LEFT SWAP !");
+                let mut left = current_grid.clone();
+                left[pos.0][pos.1] = left[pos.0][pos.1 - 1];
+                left[pos.0][pos.1 - 1] = 0;
+                swaps.push(Node::new(
+                    left,
+                    Some(parent.clone()),
+                    &goal,
+                    &self.heuristique,
+                ));
+            }
+
+            // println!("SWAPS | {} : {:?}", swaps.len(), swaps);
+
+            self.close_list.push(parent);
+            self.open_list.extend(swaps);
+            self.open_list.sort_by(|a, b| a.partial_cmp(b).unwrap());
+            // println!("NPUZZLE : {:?}", self);
+        };
+
+        println!("RESOLVED : {:?}", solved);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_goals() {
+        assert_eq!(
+            NPuzzle::generate_goal(&3, &vec![vec![3, 1, 5], vec![4, 2, 6], vec![0, 8, 7]]),
+            vec![vec![1, 2, 3], vec![8, 0, 4], vec![7, 6, 5]]
+        );
     }
 }
