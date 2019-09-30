ac3f8abe0c1097d1ff327365c48caadeb0463d89
Vincent LAY <vlay@e1r5p18.42.fr>
2019-09-30 22:03:33 +0200

    greedy

--- src/heuristique.rs
+++ src/heuristique.rs
@@ -21,9 +21,9 @@ impl Heuristique {
         }
     }
 
-    pub fn process_h(&self, grid: &[Vec<i64>], goal: &[Vec<i64>]) -> Vec<Vec<f64>> {
+    pub fn process_h(&self, grid: &[Vec<i64>], goal: &[Vec<i64>]) -> f64 {
         match &self {
-            Heuristique::Hamming => Self::process_hamming(grid, goal),
+            Heuristique::Hamming => Self::process_manhattan(grid, goal),
             Heuristique::Manhattan => Self::process_manhattan(grid, goal),
             Heuristique::LinearConflict => Self::process_linearconflict(grid, goal),
         }
